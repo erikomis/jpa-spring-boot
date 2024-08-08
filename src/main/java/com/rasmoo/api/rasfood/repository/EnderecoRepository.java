4454ef9b7a91e9fc8390f670aa085aa1c7ea7ad9
erik-martins <erik.ozzetti.martins@gmail.com>
2024-08-08 18:39:09 -0300

    feat:  controller e repository endereco

---
+++ src/main/java/com/rasmoo/api/rasfood/repository/EnderecoRepository.java
@@ -0,0 +1,13 @@
+package com.rasmoo.api.rasfood.repository;
+
+
+import com.rasmoo.api.rasfood.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco,Integer> {
+    List<Endereco> findByCep(String cep);
+}
